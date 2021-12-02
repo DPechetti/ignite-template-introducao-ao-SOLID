ce0a2ce1f3dc8ccf687bc17b3ec889ea8f96236e
Donovan Pechetti <dovi_pf@hotmail.com>
2021-12-02 08:03:59 -0300

    fix create user controller

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -5,8 +5,16 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  handle(req: Request, res: Response): Response {
+    const { name, email } = req.body;
+
+    try {
+      const userCreated = this.createUserUseCase.execute({ name, email });
+
+      return res.status(201).json(userCreated);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
   }
 }
 
