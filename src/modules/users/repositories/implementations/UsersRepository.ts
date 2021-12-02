b9062e692a691915791068627cd0504931da7bd3
Donovan Pechetti <dovi_pf@hotmail.com>
2021-12-02 07:56:45 -0300

    fix user turn admin on repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -46,7 +46,7 @@ class UsersRepository implements IUsersRepository {
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    return Object.assign(receivedUser, { admin: true, updated_at: new Date() });
   }
 
   list(): User[] {
