4364ebe0087d2832a16055065119cb195087c5ae
Donovan Pechetti <dovi_pf@hotmail.com>
2021-12-03 00:22:59 -0300

    fix show user profile controller created

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,15 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const userProfileFound = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.status(200).json(userProfileFound);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
