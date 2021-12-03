3cc682340b9f78650d6574f4e7a3a23943048b2c
Donovan Pechetti <dovi_pf@hotmail.com>
2021-12-03 01:35:16 -0300

    fix list all users controller

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const usersFound = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.status(200).json(usersFound);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
